583d41bd8f3bac0a3a3f238e8fbd3a13d6d18b26
Igor Pidik <igorPidik@gmail.com>
2022-04-15 21:14:03 +0200

    init

---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/harranali/authority"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type App struct {
+	Router    *mux.Router
+	DB        *gorm.DB
+	Authority *authority.Authority
+}
+
+func (a *App) Initialize() {
+	db, err := gorm.Open(sqlite.Open("test.db"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.DB = db
+	a.Router = mux.NewRouter()
+
+	a.InitializeRoutes()
+	a.Migrate()
+	a.InitializeAuthority()
+}
+
+func (a *App) Migrate() {
+	a.DB.AutoMigrate(&Post{})
+}
+
+func (a *App) InitializeAuthority() {
+	a.Authority = authority.New(authority.Options{
+		TablesPrefix: "authority_",
+		DB:           a.DB,
+	})
+}
+func (a *App) InitializeRoutes() {
+	a.Router.HandleFunc("/posts", a.getPosts).Methods("GET")
+	a.Router.HandleFunc("/posts", a.createPost).Methods("POST")
+	a.Router.HandleFunc("/posts/{id:[0-9]+}", a.getPost).Methods("GET")
+	a.Router.HandleFunc("/posts/{id:[0-9]+}", a.updatePost).Methods("PATCH")
+	a.Router.HandleFunc("/posts/{id:[0-9]+}", a.deletePost).Methods("DELETE")
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func (a *App) Run() {
+	log.Fatal(http.ListenAndServe(":8010", logRequest(a.Router)))
+}
+
+func (a *App) getPosts(w http.ResponseWriter, r *http.Request) {
+	var posts []Post
+	a.DB.Find(&posts)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(posts)
+}
+
+type createPostRequest struct {
+	Title string `json:"title" validate:"required"`
+	Body  string `json:"body" validate:"required"`
+}
+
+func (a *App) createPost(w http.ResponseWriter, r *http.Request) {
+	input := &createPostRequest{}
+	if err := json.NewDecoder(r.Body).Decode(input); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err := validate.Struct(input)
+	validationErrors := err.(validator.ValidationErrors)
+	fmt.Println(validationErrors)
+
+	newPost := &Post{Title: input.Title, Body: input.Body}
+
+	a.DB.Create(newPost)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newPost)
+}
+
+func (a *App) updatePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postIdString := vars["id"]
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("Error converting id")
+		return
+	}
+
+	input := &createPostRequest{}
+	if err := json.NewDecoder(r.Body).Decode(input); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	valErr := validate.Struct(input)
+	validationErrors := valErr.(validator.ValidationErrors)
+	fmt.Println(validationErrors)
+
+	updatePost := &Post{Model: gorm.Model{ID: uint(postId)}}
+	updateData := Post{Title: input.Title, Body: input.Body}
+
+	a.DB.Model(updatePost).Updates(updateData)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatePost)
+}
+
+func (a *App) getPost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postIdString := vars["id"]
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("Error converting id")
+		return
+	}
+
+	post := &Post{}
+	dbErr := a.DB.First(post, postId).Error
+	fmt.Println(dbErr)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(post)
+}
+
+func (a *App) deletePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postIdString := vars["id"]
+	postId, err := strconv.Atoi(postIdString)
+	if err != nil {
+		fmt.Println("Error converting id")
+		return
+	}
+
+	result := a.DB.Delete(&Post{}, postId)
+	fmt.Println(result.RowsAffected)
+	fmt.Println(result.Error)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+}
+
+var validate *validator.Validate
+
+func main() {
+	fmt.Println("Starting")
+
+	validate = validator.New()
+
+	a := App{}
+	a.Initialize()
+	a.Run()
+	sqlDB, err := a.DB.DB()
+	if err == nil {
+		sqlDB.Close()
+	}
+}
---
+++ post.go
@@ -0,0 +1,9 @@
+package main
+
+import "gorm.io/gorm"
+
+type Post struct {
+	gorm.Model
+	Title string
+	Body  string
+}
